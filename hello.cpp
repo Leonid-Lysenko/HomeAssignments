b8138bd6228465b5208f6defdcd4fa97b4b5e5ca
Leonid Lysenko <st128618@student.spbu.ru>
2024-09-15 09:25:32 +0300

    Fixed some outher bugs

--- hello.cpp
+++ hello.cpp
@@ -5,11 +5,13 @@
 #include <iostream>
 #include <string>
 
-void hello_friend():
+void hello_friend()
 {
+	std::cout << "Hello, World!" << std::endl;
 	std::string name;
-	
-	std::getline(std::cin, name);
-	
-	std::cout << "Hello, " << name << "! You're wonderful person ;)" << std::endl;
+	while (true)
+	{
+		getline(std::cin, name);
+		std::cout << "Hello, " << name << "! You're wonderful person ;)" << std::endl;
+	}
 }
