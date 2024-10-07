2ed1c062ba018cd82ee34aa5d83ccb3d5bc75914
leo <vboxuser@VM-1.0.myguest.virtualbox.org>
2024-10-07 17:28:57 +0300

    Created Assignment2a. It contains main1.cpp, Makefile and README.md. The program reverses the binary record and writes it to a new file.

---
+++ Assignment2a/README.md
@@ -0,0 +1,6 @@
+# Assignmet2a
+# Лысенко Леонид Максимович, 24.Б82-мм
+# st128618@student.spbu.ru
+# make
+# ./Assignment2a
+# You should go to HomeAssignments/Assignment2a, create a "source" file and write text in it. After starting the program, a "temppdf" file will appear with upside down text.
---
+++ Assignment2a/main1.cpp
@@ -0,0 +1,45 @@
+/* Leonid Lysenko st128618@student.spbu.ru
+   Assignment2a
+*/
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+
+int main()
+{
+    /*
+    We create an array to work with it.
+    */
+    unsigned fileSize = std::filesystem::file_size("source");
+    char* buffer = new char[fileSize];
+    std::ifstream infile;
+    infile.open("source", std::ios::binary | std::ios::in);
+    /*
+    We read the file to work with it.
+    */
+    int Fsize = 0;
+    while (infile >> buffer[Fsize])
+    {
+        Fsize++;
+    }
+    infile.close();
+    /*
+    We reverse the line elements in the file.
+    */
+    for (int i {0}; i < Fsize / 2; i++)
+    {
+        std::swap(buffer[i], buffer[Fsize - (i+1)]);
+    }
+    /*
+    We write the reversed string to a new file.
+    */
+    std::ofstream outfile;
+    outfile.open("temppdf", std::ios::binary | std::ios::out);
+    outfile.write((char *)buffer, Fsize * sizeof(buffer[0]));
+    outfile.close();
+    /*
+    We perform memory cleaning.
+    */
+    delete[] buffer;
+}
