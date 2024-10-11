554f1e48c8946d7b21005215e5c40c689df10f60
leo <vboxuser@VM-1.0.myguest.virtualbox.org>
2024-10-11 23:20:25 +0300

    Reading the entire file

--- Assignment2a/main1.cpp
+++ Assignment2a/main1.cpp
@@ -11,35 +11,28 @@ int main()
     /*
     We create an array to work with it.
     */
-    unsigned fileSize = std::filesystem::file_size("source");
+    unsigned fileSize = std::filesystem::file_size("source")-1;
     char* buffer = new char[fileSize];
     std::ifstream infile;
     infile.open("source", std::ios::binary | std::ios::in);
     /*
     We read the file to work with it.
     */
-    int Fsize = 0;
-    char ch;
-    while (infile.get(ch))
-    {
-        buffer[Fsize] = ch;
-        Fsize++;
-    }
-    Fsize--;
+    infile.read(buffer, fileSize);
     infile.close();
     /*
     We reverse the line elements in the file.
     */
-    for (int i {0}; i < Fsize / 2; i++)
+    for (int i {0}; i < fileSize / 2; i++)
     {
-        std::swap(buffer[i], buffer[Fsize - (i+1)]);
+        std::swap(buffer[i], buffer[fileSize - (i+1)]);
     }
     /*
     We write the reversed string to a new file.
     */
     std::ofstream outfile;
     outfile.open("temppdf", std::ios::binary | std::ios::out);
-    outfile.write((char *)buffer, Fsize * sizeof(buffer[0]));
+    outfile.write((char *)buffer, fileSize * sizeof(buffer[0]));
     outfile.close();
     /*
     We perform memory cleaning.
